e78b568e992ea66e705742e278e409bb09b2abca
fisayo-dev <olufisayobadina@gmail.com>
2026-03-04 20:37:09 +0100

    feat: change metadata of login page

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,17 +1,37 @@
-import { ReactNode } from "react"
+import { Metadata } from "next";
+import { ReactNode } from "react";
 
-export const metadata = {
-    title: 'Login | Scanzie',
-    description: 'Login in to your Scanzie account.',
-    keywords: ['login', 'authentication', 'secure', 'Scanzie', 'seo', 'analyze', 'user access'],
-    robots: 'noindex, nofollow',
-    charset: 'utf-8',
-} 
-const layout = ({children}: {children:ReactNode}) => {
+export const metadata: Metadata = {
+  title: "Login | Scanzie",
+  description: "Login in to your Scanzie account.",
+  keywords: [
+    "login",
+    "authentication",
+    "secure",
+    "Scanzie",
+    "seo",
+    "analyze",
+    "user access",
+  ],
+  robots: "noindex, nofollow",
+  openGraph: {
+    title: "Login | Scanzie",
+    description:
+      "Login in to your Scanzie account.",
+    type: "website",
+    siteName: "Scanzie",
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Login | Scanzie",
+    description:
+      "Login in to your Scanzie account.",
+    site: "@scanzie", // Add your Twitter handle
+  },
+};
+const layout = ({ children }: { children: ReactNode }) => {
+  return <div>{children}</div>;
+};
 
-  return (
-    <div>{children}</div>
-  )
-}
-
-export default layout
+export default layout;
