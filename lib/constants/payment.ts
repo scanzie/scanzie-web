3e4892483d3b02028f7c8996ef1cb75f8260788a
fisayo-dev <olufisayobadina@gmail.com>
2026-03-04 21:28:25 +0100

    refactor: add constants to store important payment info

--- lib/constants/payment.ts
+++ lib/constants/payment.ts
@@ -1,6 +1,18 @@
+// Urls
+export const PAYMENT_SUCCESSFUL_CALLBACK_URL =
+  "/dashboard?successfully-subscribed=true";
+export const PAYSTACK_VERIFY_TRANSACTION_URL =
+  "https://api.paystack.co/transaction/verify";
+export const PAYSTACK_INITIALIZE_TRANSACTION_URL =
+  "https://api.paystack.co/transaction/initialize";
+
+// Keywords
 export const CREATE_SUBSCRIPTION = "subscription.create";
 export const PAYMENT_FAILED = "invoice.payment_failed";
 export const PAYMENT_CALLBACK_MAX_POLLS = 12;
-export const PAYMENT_SUCCESSFUL_CALLBACK_URL =
-  "/dashboard?successfully-subscribed=true";
-export const PAYSTACK_VERIFY_TRANSACTION_URL = "https://api.paystack.co/transaction/verify";
+
+// Plan Prices
+export const SCANZIE_PRO_MONTHLY = 16500 * 100; // $12.00
+export const SCANZIE_PRO_YEARLY = 149760 * 100; // $115.20
+export const SCANZIE_BUSINESS_MONTHLY = 97500 * 100; // $75.00
+export const SCANZIE_BUSINESS_YEARLY = 936000 * 100; // $720.00
