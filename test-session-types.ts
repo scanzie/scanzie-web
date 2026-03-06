ce38c0647904338023ab44ddf22351b8791dc24e
fisayo-dev <olufisayobadina@gmail.com>
2026-03-06 14:16:40 +0100

    chore: remove semi-colon

--- test-session-types.ts
+++ test-session-types.ts
@@ -1,2 +1,2 @@
-import { authClient } from "@/lib/auth/client";
+import { authClient } from "@/lib/auth/client"
 type T = typeof authClient.getSession extends (...args: any) => Promise<{ data: infer D }> ? D : never;
