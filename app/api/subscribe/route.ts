9ce086b8f7454c383e047729bb629f5b5cfbfaf0
fisayo-dev <olufisayobadina@gmail.com>
2026-03-06 12:30:26 +0100

    chore: add payment channels to subscribe route

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -47,6 +47,7 @@ export async function POST(req: NextRequest) {
       amount: subscriptionAmount,
       plan: planCode,
       callback_url: callbackUrl.toString(),
+      channels: ["card", "bank", "apple_pay", "ussd", "qr", "mobile_money", "bank_transfer", "eft", "capitec_pay", "payattitude"]
     }),
   });
 
