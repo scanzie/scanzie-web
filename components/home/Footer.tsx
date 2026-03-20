2854b871f4dc2aab18a275d2706144037548e356
fisayo-dev <olufisayobadina@gmail.com>
2026-03-20 17:27:15 +0100

    chore: add facebook link

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -55,13 +55,8 @@ const Footer: React.FC = () => {
                   </Link>
                 </li>
                 <li>
-                  <Link href="#" className="text-sm text-gray-600 hover:text-blue-600 transition-colors">
+                  <Link href="https://web.facebook.com/profile.php?id=61578661535793" className="text-sm text-gray-600 hover:text-blue-600 transition-colors">
                     Facebook
-                  </Link>
-                </li>
-                <li>
-                  <Link href="#" className="text-sm text-gray-600 hover:text-blue-600 transition-colors">
-                    Instagram
                   </Link>
                 </li>
                 <li>
